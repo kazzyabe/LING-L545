5f6f7e846506faff1b12ad1263422f50c57989ad
Kazuki Yabe <kyabe@iu.edu>
2019-09-03 15:33:12 -0400

    Reverse is added to FinalConsSeq.sh

--- 00_Unix/FinalConsSeq.sh
+++ 00_Unix/FinalConsSeq.sh
@@ -1,3 +1,3 @@
 grep '[^aeiouAEIOU]$' < wiki.words | rev > fconswords.txt
-gsed 's/[aeiouAEIOU].*//g' < fconswords.txt > fconsequence.txt
+gsed 's/[aeiouAEIOU].*//g' < fconswords.txt | rev > fconsequence.txt
 uniq -c < consequence.txt > fseqcount.txt
